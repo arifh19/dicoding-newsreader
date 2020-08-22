6abf884dbb08ebd73e4d78cd543dc77386f7f4ed
Arif Hidayat <arif.h@mail.ugm.ac.id>
2020-08-22 18:59:22 +0700

    first commit

--- service-worker.js
+++ service-worker.js
@@ -6,11 +6,11 @@ else
   console.log("Workbox gagal dimuat")
 
 workbox.precaching.precacheAndRoute([
-  { url: '/index.html', revision: '1'},
-  { url: '/nav.html', revision: '1'},
-  { url: '/css/materialize.min.css', revision: '1'},
-  { url: '/js/materialize.min.js', revision: '1'},
-  { url: '/js/script.js', revision: '1'},
+  { url: 'index.html', revision: '1'},
+  { url: 'nav.html', revision: '1'},
+  { url: 'css/materialize.min.css', revision: '1'},
+  { url: 'js/materialize.min.js', revision: '1'},
+  { url: 'js/script.js', revision: '1'},
 ])
 workbox.routing.registerRoute(
   new RegExp('/pages/'),
